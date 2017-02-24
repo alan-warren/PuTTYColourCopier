ddfec0cd69a23eb12bb5fa6e989a0f1289cfee06
Alan Warren <awarren@uoguelph.ca>
2017-02-24 17:00:41 -0500

    Trying out powershell registry stuff with an eye to gussying up my PuTTY colour schemes

---
+++ PuTTY_ColourSchemeCopy.ps1
@@ -0,0 +1,48 @@
+﻿[CmdletBinding(SupportsShouldProcess=$true)]
+param([Object]$Objects)
+$scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+. "$scriptPath/Select-Item.ps1"
+
+
+$sessionList = Get-Item -Path Registry::HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\Sessions
+$sessionNames = $sessionList.GetSubKeyNames();
+$colourCount = 21
+
+<# 
+    If you want to copy fields other than colours from one profile to others, you can redefine this array
+    to start off non-empty
+    ex. $fieldsToCopy = "BeepInd", "Font"
+#>
+$fieldsToCopy = @()
+
+
+$templateNum = Select-Item -Caption "PuTTY Sesssion Colour Template" -Message "Choose the PuTTY session to source colours from" -choiceList $sessionNames
+$templateSession = $sessionList.OpenSubKey($sessionNames[$templateNum])
+$srcKeyValuePair = @{}
+
+#Add the colours to the list of fields we'll be copying. Pretty inefficient, but only do it once per script exec.
+for($i = 0; $i -le $colourCount; $i++){
+    $fieldsToCopy += "Colour$i"
+}
+#Copy the key-value pairs we want so we don't keep going to the registry
+foreach($field in $fieldsToCopy){
+    $srcKeyValuePair[$field] = $templateSession.GetValue($field)
+}
+echo "Using colours from $($templateSession.ToString())"
+$templateSession.Close()
+Remove-Item $scriptPath\testFile.txt
+
+foreach($sess in $sessionNames) {
+    $doIt = Read-Host -Prompt "Apply colour scheme to $($sess) (Y/N)?"
+    if($doIt.trim().ToUpper()[0] -eq "Y"){
+        echo "Doing it!"
+        $targetSession = $sessionList.OpenSubKey($sess)
+        foreach($key in $srcKeyValuePair.Keys){
+            $curVal = $targetSession.GetValue($field);
+            $newVal = $srcKeyValuePair[$key]
+            echo "Current value is $curVal"
+            echo "Template value is $newVal"
+        }
+        $targetSession.close()
+    }
+}
