f14e5056d8749f9f45b56a557b2e600f7f961d7a
Alan Warren <awarren@uoguelph.ca>
2017-04-23 00:53:26 -0400

    Avoid aliases, actually write values to target sessions, and generate my own select menu since setting up hotkeys would be a pain in the butt

--- PuTTY_ColourSchemeCopy.ps1
+++ PuTTY_ColourSchemeCopy.ps1
@@ -6,7 +6,7 @@ $scriptPath = Split-Path -parent $MyInvocation.MyCommand.Definition
 Try {
     $sessionList = Get-Item -Path Registry::HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\Sessions -ErrorAction Stop
 } Catch {
-    echo "No sessions in registry"
+    Write-Output "No sessions in registry"
     exit
 }
 $sessionNames = $sessionList.GetSubKeyNames()
@@ -18,9 +18,26 @@ $colourCount = 21
     ex. $fieldsToCopy = "BeepInd", "Font"
 #>
 $fieldsToCopy = @()
-
-$templateNum = Select-Item -Caption "PuTTY Sesssion Colour Template" -Message "Choose the PuTTY session to source colours from" -choiceList $sessionNames
-$templateSession = $sessionList.OpenSubKey($sessionNames[$templateNum])
+$ctr = 0
+$colWidth = 3
+$outLine = ""
+foreach($session in $sessionNames) {
+    $ctr++
+    $outLine += "[$ctr] $session`t"
+    if($ctr % $colWidth -eq 0){ 
+        Write-Output $objLine
+        $outLine = ""
+    }
+}
+if($ctr % $colWidth -ne 0){
+    #Output the extra value
+    Write-Output $outLine
+}
+$choiceStr = Read-Host -Prompt "Pick a session to copy from"
+$choice = [Convert]::ToInt32($choiceStr)
+$choice--
+$templateSessionName = $sessionNames[$choice]
+$templateSession = $sessionList.OpenSubKey($templateSessionName)
 $srcKeyValuePair = @{}
 
 #Add the colours to the list of fields we'll be copying. Pretty inefficient, but only do it once per script exec.
@@ -31,21 +48,27 @@ for($i = 0; $i -le $colourCount; $i++){
 foreach($field in $fieldsToCopy){
     $srcKeyValuePair[$field] = $templateSession.GetValue($field)
 }
-echo "Using colours from $($templateSession.ToString())"
+Write-Output "Using colours from $($templateSession.ToString())"
 $templateSession.Close()
 #this is in here to test if the -WhatIf automatically flows to called cmdlets
-Remove-Item $scriptPath\testFile.txt
+#Remove-Item $scriptPath\testFile.txt
 
 foreach($sess in $sessionNames) {
+    if($sess -eq $templateSessionName) {
+        continue
+    }
     $doIt = Read-Host -Prompt "Apply colour scheme to $($sess) (Y/N)?"
     if($doIt.trim().ToUpper()[0] -eq "Y"){
-        echo "Doing it!"
-        $targetSession = $sessionList.OpenSubKey($sess)
+        Write-Output "Doing it!"
+        $targetSession = $sessionList.OpenSubKey($sess, $true)
         foreach($key in $srcKeyValuePair.Keys){
-            $curVal = $targetSession.GetValue($field);
+            Write-Output $key
+            $curVal = $targetSession.GetValue($key);
             $newVal = $srcKeyValuePair[$key]
-            echo "Current value is $curVal"
-            echo "Template value is $newVal"
+            Write-Output "Current value is $curVal"
+            Write-Output "Template value is $newVal"
+            $targetSession.SetValue($key, $newVal)
+            $curVal = $targetSession.GetValue($key)
         }
         $targetSession.close()
     }
